f0b83205f5e3c874703ed7a438fbaa469d27df78
Tomasz Wiśniewski <tomasz@wisniewski.app>
2025-01-10 19:31:29 +0100

    Fix vector out of range errors

--- server.cpp
+++ server.cpp
@@ -91,8 +91,8 @@ struct Player
             }
             auto rng = std::default_random_engine{};
             std::vector<std::string> temp{};
-            std::move(cards_facing_up.begin(), cards_facing_up.end() - 1, std::back_inserter(temp));
-            cards_facing_up.erase(cards_facing_up.begin(), cards_facing_up.end() - 1);
+            std::move(cards_facing_up.begin(), cards_facing_up.end(), std::back_inserter(temp));
+            cards_facing_up.erase(cards_facing_up.begin(), cards_facing_up.end());
             std::shuffle(temp.begin(), temp.end(), rng);
             std::move(temp.begin(), temp.end(), std::back_inserter(cards_facing_down));
         }
@@ -345,6 +345,10 @@ public:
 
         for (const auto &player : get_players())
         {
+            if (player->cards_facing_up.size() <= 1)
+            {
+                continue;
+            }
             std::move(player->cards_facing_up.begin(), player->cards_facing_up.end() - 1, std::back_inserter(cards_to_be_distributed));
             player->cards_facing_up.erase(player->cards_facing_up.begin(), player->cards_facing_up.end() - 1);
         }
