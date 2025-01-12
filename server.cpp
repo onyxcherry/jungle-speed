c297f6a79a7451d9cbec8b671dd8572db4e8e58d
Tomasz Wiśniewski <tomasz@wisniewski.app>
2025-01-12 15:40:45 +0100

    Format

--- server.cpp
+++ server.cpp
@@ -115,23 +115,28 @@ struct Player
         return std::make_pair(cards_facing_up.size(), cards_facing_down.size());
     }
 
-    std::string get_username() {
+    std::string get_username()
+    {
         return username;
     }
 
-    int get_fd() {
+    int get_fd()
+    {
         return fd;
     }
 
-    int get_position() {
+    int get_position()
+    {
         return position_in_game;
     }
 
-    void set_game_id(int id) {
+    void set_game_id(int id)
+    {
         game_id = id;
     };
 
-    int get_game_id() {
+    int get_game_id()
+    {
         return game_id;
     }
 };
@@ -152,6 +157,7 @@ class Game
     bool next_card_turned_up = false;
     std::unordered_map<std::string, int> cards_repeats{};
     std::string owner_name;
+
 public:
     // changing totem_held_by needs locked totem_mtx
     std::mutex totem_mtx;
@@ -182,24 +188,29 @@ public:
         return false;
     }
 
-    bool remove_player(int player) {
+    bool remove_player(int player)
+    {
         auto it = players.find(player);
-        if (it != players.end()) {
+        if (it != players.end())
+        {
             players.erase(it);
             std::cout << "Player with ID " << player << " has been removed.\n";
             return true;
-        } else {
+        }
+        else
+        {
             std::cout << "Player with ID " << player << " not found.\n";
             return false;
         }
     }
 
-    //May need mutex?
-    void set_owner(std::string name) {
+    void set_owner(std::string name)
+    {
         owner_name = name;
     }
 
-    std::string get_owner() {
+    std::string get_owner()
+    {
         return owner_name;
     }
 
@@ -213,7 +224,8 @@ public:
         return players_to_return;
     }
 
-    std::string cards_state(Player &player) {
+    std::string cards_state(Player &player)
+    {
         Player &p = *get_player(player.fd);
         std::string up = p.cards_facing_up.size() > 0 ? "true" : "false";
         std::string down = p.cards_facing_down.size() > 0 ? "true" : "false";
@@ -438,7 +450,7 @@ private:
     std::vector<std::string> generate_cards()
     {
         return std::vector<std::string>{
-            "circle_inside_x_red", 
+            "circle_inside_x_red",
             "circle_inside_x_red",
             "circle_inside_x_blue",
             "circle_inside_x_yellow",
@@ -446,8 +458,7 @@ private:
             "circle_inside_x_red",
             "circle_inside_x_yellow",
             "circle_inside_x_yellow",
-            "circle_inside_x_yellow"
-        };
+            "circle_inside_x_yellow"};
     }
 
     std::unordered_map<std::string, int> count_cards_repeats()
@@ -625,7 +636,7 @@ private:
     }
 
     std::string extract_message(std::vector<char> &data)
-    {  
+    {
 
         auto it = std::search(data.begin(), data.end(), msg_end_marker.begin(), msg_end_marker.end());
         if (it == data.end())
@@ -650,7 +661,7 @@ private:
             return;
         }
         player.msg_in.insert(player.msg_in.end(), buffer, buffer + bytes_received);
-        
+
         std::string s(player.msg_in.begin(), player.msg_in.end());
         std::string message = extract_message(player.msg_in);
 
@@ -672,16 +683,14 @@ private:
     }
 
     void send_messages_to_client(Player &player)
-    {   
+    {
         std::lock_guard<std::mutex> lock(player.msg_mtx);
         std::cout << "Msg for clinet: " << std::endl;
         for (char c : player.msg_out)
         {
             std::cout << c;
         }
-         std::cout << std::endl;
-
-
+        std::cout << std::endl;
 
         int sent_bytes = send(player.fd, player.msg_out.data(), player.msg_out.size(), 0);
         if (sent_bytes <= 0)
@@ -801,9 +810,9 @@ private:
             {"middle", in_middle_count},
         };
 
-
-        for(auto &p : game.get_players()){
-            message[std::to_string(p->fd)] =  game.cards_state(*p);
+        for (auto &p : game.get_players())
+        {
+            message[std::to_string(p->fd)] = game.cards_state(*p);
         }
         send_game_update(*player, "CARDS_COUNTS", message);
     }
@@ -833,7 +842,7 @@ private:
 
         if (action == "LIST_GAMES")
         {
-            std::cout << "Player" <<  player.fd << " requested list of games" << std::endl;
+            std::cout << "Player" << player.fd << " requested list of games" << std::endl;
             json response = list_games();
             action = "UPDATE_LOBBIES";
             send_success(player, action, response);
@@ -847,11 +856,14 @@ private:
                 send_success(player, action, response);
                 action = "UPDATE_LOBBIES";
                 response = list_games();
-                for (const auto &p : players_out_of_games) {
-                    std::cout << "sending update to" <<  p->fd << std::endl;
+                for (const auto &p : players_out_of_games)
+                {
+                    std::cout << "sending update to" << p->fd << std::endl;
                     send_success(*p, action, response);
                 }
-            } else {
+            }
+            else
+            {
                 send_error(player, action, response);
             }
         }
@@ -865,11 +877,14 @@ private:
                 update_players_in_lobby(player, action, response);
                 action = "UPDATE_LOBBIES";
                 response = list_games();
-                for (const auto &p : players_out_of_games) {
-                    std::cout << "sending update to" <<  p->fd << std::endl;
+                for (const auto &p : players_out_of_games)
+                {
+                    std::cout << "sending update to" << p->fd << std::endl;
                     send_success(*p, action, response);
                 }
-            } else {
+            }
+            else
+            {
                 send_error(player, action, response);
             }
         }
@@ -882,10 +897,14 @@ private:
         {
             auto [success, response] = catch_totem(player);
             (success) ? send_success(player, action, response) : send_error(player, action, response);
-        } else if (action == "GET_USERNAME") {
+        }
+        else if (action == "GET_USERNAME")
+        {
             auto [success, response] = get_username(player);
             (success) ? send_success(player, action, response) : send_error(player, action, response);
-        } else if(action == "START_GAME") {
+        }
+        else if (action == "START_GAME")
+        {
             start_game_by_player(player);
         }
         else
@@ -902,9 +921,10 @@ private:
         t.detach();
     }
 
-    std::pair<bool, json> get_username(Player &player) {
+    std::pair<bool, json> get_username(Player &player)
+    {
 
-        json response = {{"username",player.get_username()}, {"id", player.get_fd()}};
+        json response = {{"username", player.get_username()}, {"id", player.get_fd()}};
 
         return make_pair(true, response);
     }
@@ -920,10 +940,10 @@ private:
         send_to_all(game.get_players(), "START", start_info);
 
         // Set players card in beggining
-            for (auto &player : game.get_players())
-            {
-                send_cards_count(player, game.get_middle_cards_count(), game);
-            }
+        for (auto &player : game.get_players())
+        {
+            send_cards_count(player, game.get_middle_cards_count(), game);
+        }
         // Code to show player whose turn is it in begining
         send_next_turn(game.get_players(), game.get_current_turn_player().username);
 
@@ -949,14 +969,14 @@ private:
                 }
 
                 if (has_outwards_arrows(current_card))
-                {   
+                {
                     std::cout << "Oborcenie kart" << std::endl;
                     json one_message = json::object();
                     std::cout << "Stworzono one msg" << std::endl;
 
                     std::set<std::string> turned_up_cards;
                     bool duel = false;
-                    
+
                     std::cout << "Przed loopa" << std::endl;
                     for (const auto &player : game.get_players())
                     {
@@ -974,18 +994,14 @@ private:
                         turned_up_cards.insert(card);
                         std::cout << "Probuje ustawic w one msg, card i fd" << std::endl;
 
-
                         one_message[std::to_string(player->fd)] = card;
                         std::cout << "Udalo sie" << std::endl;
-
                     }
                     std::cout << "Wyszlismy" << std::endl;
 
                     send_to_all(game.get_players(), "OUTWARDS_ARROWS_TURNED_CARDS", one_message);
                     std::cout << "Wyslano outwards info" << std::endl;
 
-
-                    
                     if (!duel)
                     {
                         std::cout << "Nie ma  duel" << std::endl;
@@ -1026,10 +1042,8 @@ private:
 
                     game.set_turn_of(losers.back()->fd);
                     std::cout << "Set turn of zrobiony" << std::endl;
-
                 }
                 std::cout << "Poza outwards " << std::endl;
-
             }
             else if (game.totem_cv->wait_for(totem_lock, max_waiting_ms_mistakenly_hold_totem, [&game]()
                                              { return game.is_totem_held(); }))
@@ -1077,7 +1091,7 @@ private:
                               {"is_started", game.has_been_started()}};
             response["games"].push_back(game_info);
         }
-        std::cout << "Curr games: " <<  response.dump() << std::endl;
+        std::cout << "Curr games: " << response.dump() << std::endl;
         return response;
     }
 
@@ -1113,24 +1127,24 @@ private:
 
         game.set_owner(player.username);
 
-        //Adding player to players in game
+        // Adding player to players in game
         players_in_games_list.push_back(player_ptr);
 
         remove_player_from_waiting_list(player.fd);
-        
-        //Setting up time of joining lobby and ownership (done in game struct but did it in case)
+
+        // Setting up time of joining lobby and ownership (done in game struct but did it in case)
         player.join_lobby_time = std::chrono::steady_clock::now();
         player.is_owner = true;
         player.set_game_id(game.get_identifier());
 
-
         auto [names, fds] = sort_by_time(game.get_players(), player);
-        json response = {{"game_id", game_id},{"usernames", names},{"position",player.get_position()},{"owner",game.get_owner()},{"fds",fds}};
+        json response = {{"game_id", game_id}, {"usernames", names}, {"position", player.get_position()}, {"owner", game.get_owner()}, {"fds", fds}};
 
         return make_pair(true, response);
     }
 
-    std::pair<bool, json> leave_lobby(Player &player, const json &message) {
+    std::pair<bool, json> leave_lobby(Player &player, const json &message)
+    {
         if (!message.contains("game_id"))
         {
             json response = {{"error", "Missing 'game_id' field."}};
@@ -1161,10 +1175,8 @@ private:
         remove_player_to_in_game_list(player.fd);
         json response = {{"game_id", game_id}};
 
-
         return make_pair(true, response);
     }
-
 
     std::pair<bool, json> join_game(Player &player, const json &message)
     {
@@ -1213,17 +1225,15 @@ private:
         auto player_ptr = *player_it;
         player_ptr->join_lobby_time = std::chrono::steady_clock::now();
 
-
         players_in_games.insert(std::make_pair(player.fd, game.get_identifier()));
         game.add_player(player_ptr);
         remove_player_from_waiting_list(player.fd);
 
-        auto [names, fds] =  sort_by_time(game.get_players(), player);
+        auto [names, fds] = sort_by_time(game.get_players(), player);
 
         player.set_game_id(game.get_identifier());
 
-
-        json response = {{"game_id", game_id},{"usernames", names},{"position", player.get_position()},{"owner", game.get_owner()}, {"fds",fds}};
+        json response = {{"game_id", game_id}, {"usernames", names}, {"position", player.get_position()}, {"owner", game.get_owner()}, {"fds", fds}};
         std::cout << "Usarenames in lobby: " << names << std::endl;
         return make_pair(true, response);
     }
@@ -1295,34 +1305,35 @@ private:
             players_out_of_games.end());
     }
 
-    void remove_player_to_in_game_list(int client_fd) {
+    void remove_player_to_in_game_list(int client_fd)
+    {
         players_in_games_list.erase(
             std::remove_if(players_in_games_list.begin(), players_in_games_list.end(), [client_fd](std::shared_ptr<Player> const player)
                            { return player->fd == client_fd; }),
-        players_in_games_list.end());
+            players_in_games_list.end());
     };
 
-
-    //Funtcion to order players by time of joining
-    std::pair<std::string, std::string> sort_by_time(std::vector<std::shared_ptr<Player>> players, Player &main_player) {
-        std::sort(players.begin(), players.end(), [](const std::shared_ptr<Player>& a, const std::shared_ptr<Player>& b) {
-            return a->join_lobby_time < b->join_lobby_time;
-        });
+    // Funtcion to order players by time of joining
+    std::pair<std::string, std::string> sort_by_time(std::vector<std::shared_ptr<Player>> players, Player &main_player)
+    {
+        std::sort(players.begin(), players.end(), [](const std::shared_ptr<Player> &a, const std::shared_ptr<Player> &b)
+                  { return a->join_lobby_time < b->join_lobby_time; });
 
         std::string result;
         std::string fd;
         int position = 0;
-        for (const auto& player : players) {
-            if(player->fd == main_player.fd) {
+        for (const auto &player : players)
+        {
+            if (player->fd == main_player.fd)
+            {
                 main_player.position_in_game = position;
             }
             std::cout << position << std::endl;
             position++;
             result += player->username;
             fd += std::to_string(player->fd);
-            result += " "; 
+            result += " ";
             fd += " ";
-
         }
         std::cout << "Pozycja gracza w lobby: " << position;
         std::cout << "Pozycja zapisana: " << main_player.get_position();
@@ -1330,23 +1341,26 @@ private:
         return std::make_pair(result, fd);
     }
 
-
-    void update_players_in_lobby(Player &main_player, std::string &action, json &response) {
+    void update_players_in_lobby(Player &main_player, std::string &action, json &response)
+    {
         Game &game = *games.at(main_player.get_game_id());
-        for (const auto &p : game.get_players()) {
-            std::cout << "In lobby: sending update to" <<  p->fd << std::endl;
-            if(main_player.fd != p->fd) {
+        for (const auto &p : game.get_players())
+        {
+            std::cout << "In lobby: sending update to" << p->fd << std::endl;
+            if (main_player.fd != p->fd)
+            {
                 send_success(*p, action, response);
             }
         }
-    } 
-    void start_game_by_player(Player &main_player) {
+    }
+    void start_game_by_player(Player &main_player)
+    {
         Game &game = *games.at(main_player.get_game_id());
-        if(game.get_owner() == main_player.get_username() && game.get_players().size()) {
+        if (game.get_owner() == main_player.get_username() && game.get_players().size())
+        {
             start_game(game);
         }
     }
-
 };
 
 int main(int argc, char *argv[])
